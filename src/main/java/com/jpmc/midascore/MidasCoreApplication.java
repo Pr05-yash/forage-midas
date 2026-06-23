e282863546b66643301481ffb37fc6136161c7da
Yash Prajapati <yashprajapati.tech@gmail.com>
2026-06-23 15:46:10 +0530

    Update MidasCoreApplication.java

--- src/main/java/com/jpmc/midascore/MidasCoreApplication.java
+++ src/main/java/com/jpmc/midascore/MidasCoreApplication.java
@@ -2,19 +2,12 @@ package com.jpmc.midascore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.jpmc.midascore") // Controller ko dhundne ke liye
 public class MidasCoreApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MidasCoreApplication.class, args);
-    }
-
-    // Yeh naya bean add karna hai
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
     }
 }
