1ba291430d59bd011fdb9d345f207ab93454d957
Yash Prajapati <yashprajapati.tech@gmail.com>
2026-06-23 14:18:52 +0530

    Fix formatting in TaskThreeTests.java

--- src/test/java/com/jpmc/midascore/TaskThreeTests.java
+++ src/test/java/com/jpmc/midascore/TaskThreeTests.java
@@ -13,7 +13,7 @@ import org.springframework.test.annotation.DirtiesContext;
 @DirtiesContext
 @EmbeddedKafka(
     partitions = 1, 
-    topics = {"midas-transactions"}, // Ye line add karein
+    topics = {"midas-transactions"}, 
     brokerProperties = { 
         "listeners=PLAINTEXT://localhost:9092", 
         "port=9092"
