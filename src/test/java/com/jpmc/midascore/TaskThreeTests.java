d179f7081f713ba03ce0a854f65bd24aa7010b76
Yash Prajapati <yashprajapati.tech@gmail.com>
2026-06-23 14:46:30 +0530

    Update TaskThreeTests.java

--- src/test/java/com/jpmc/midascore/TaskThreeTests.java
+++ src/test/java/com/jpmc/midascore/TaskThreeTests.java
@@ -1,56 +1,24 @@
 package com.jpmc.midascore;
 
-import com.jpmc.midascore.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach; // Naya import
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.annotation.DirtiesContext;
 
-@SpringBootTest(classes = MidasCoreApplication.class) // Ye change karein
+@SpringBootTest(classes = MidasCoreApplication.class)
 @DirtiesContext
-@EmbeddedKafka(
-    partitions = 1, 
-    topics = {"midas-transactions"},
-    brokerProperties = { 
-        "listeners=PLAINTEXT://localhost:9092", 
-        "port=9092"
-    }
-)
+@EmbeddedKafka(partitions = 1, topics = {"midas-transactions"})
 public class TaskThreeTests {
-    static final Logger logger = LoggerFactory.getLogger(TaskThreeTests.class);
 
-    @Autowired
-    private KafkaProducer kafkaProducer;
-    @Autowired
-    private UserPopulator userPopulator;
-    @Autowired
-    private FileLoader fileLoader;
-    @Autowired
-    private UserRepository userRepository;
-
-@Test
-void task_three_verifier() throws InterruptedException {
-    System.out.println(">>> [DEBUG] TEST START HO GAYA HAI!"); // Check karne ke liye
-    
-    userPopulator.populate();
-    Thread.sleep(2000); 
-    
-    String[] transactionLines = fileLoader.loadStrings("/test_data/mnbvcxz.vbnm");
-    for (String transactionLine : transactionLines) {
-        kafkaProducer.send(transactionLine);
+    @BeforeEach
+    void setup() {
+        System.out.println(">>> [DEBUG] SETUP CHAL GAYA HAI");
     }
-    
-    System.out.println(">>> [DEBUG] MESSAGES BHEJ DIYE HAIN, AB WAIT KAR RAHE HAIN...");
-    Thread.sleep(10000); // 10 second wait
 
-    var waldorf = userRepository.findByName("Waldorf");
-    
-    if (waldorf != null) {
-        System.out.println(">>> WALDORF FINAL BALANCE: " + (int) Math.floor(waldorf.getBalance()));
-    } else {
-        System.out.println(">>> ERROR: WALDORF NAHI MILA!");
+    @Test
+    void task_three_verifier() {
+        System.out.println(">>> [DEBUG] TEST METHOD START HO GAYA HAI");
+        // Filhal sirf yahi rakhein
     }
 }
