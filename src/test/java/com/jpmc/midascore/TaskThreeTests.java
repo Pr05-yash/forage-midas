87a3ac54a1258554c5dec46a392a28a6a80432be
Yash Prajapati <yashprajapati.tech@gmail.com>
2026-06-23 14:19:34 +0530

    Update TaskThreeTests.java

--- src/test/java/com/jpmc/midascore/TaskThreeTests.java
+++ src/test/java/com/jpmc/midascore/TaskThreeTests.java
@@ -31,27 +31,26 @@ public class TaskThreeTests {
     @Autowired
     private UserRepository userRepository;
 
-  @Test
+ @Test
 void task_three_verifier() throws InterruptedException {
-    // 1. Data populate karein
     userPopulator.populate();
     
-    // 2. Transactions load karke Kafka pe bhejein
+    // Kafka ko settle hone ka time dein
+    Thread.sleep(5000); 
+    
     String[] transactionLines = fileLoader.loadStrings("/test_data/mnbvcxz.vbnm");
     for (String transactionLine : transactionLines) {
         kafkaProducer.send(transactionLine);
     }
     
-    // 3. Kafka process hone ka wait (30 seconds)
-    System.out.println("Processing transactions...");
-  // Kafka process hone ka wait
-        Thread.sleep(10000); 
+    // Process hone ka wait
+    Thread.sleep(10000); 
 
-        // Balance fetch aur print
-        var waldorf = userRepository.findByName("Waldorf");
-        System.out.println(">>> WALDORF FINAL BALANCE: " + (int) Math.floor(waldorf.getBalance()));
+    var waldorf = userRepository.findByName("Waldorf");
+    
+    System.out.println("\n\n##########################################################");
     if (waldorf != null) {
-        System.out.println("WALDORF FINAL BALANCE IS: " + waldorf.getBalance());
+        System.out.println(">>> WALDORF FINAL BALANCE: " + (int) Math.floor(waldorf.getBalance()));
     } else {
         System.out.println("ERROR: WALDORF DATABASE MEIN NAHI MILA!");
     }
