93b68162d12cf2beb66bc1b8c8759f6a3d75cbc3
Yash Prajapati <yashprajapati.tech@gmail.com>
2026-06-23 14:47:59 +0530

    Update TaskThreeTests.java

--- src/test/java/com/jpmc/midascore/TaskThreeTests.java
+++ src/test/java/com/jpmc/midascore/TaskThreeTests.java
@@ -16,9 +16,26 @@ public class TaskThreeTests {
         System.out.println(">>> [DEBUG] SETUP CHAL GAYA HAI");
     }
 
-    @Test
-    void task_three_verifier() {
-        System.out.println(">>> [DEBUG] TEST METHOD START HO GAYA HAI");
-        // Filhal sirf yahi rakhein
+   @Test
+void task_three_verifier() throws InterruptedException {
+    userPopulator.populate();
+    
+    String[] transactionLines = fileLoader.loadStrings("/test_data/mnbvcxz.vbnm");
+    for (String transactionLine : transactionLines) {
+        kafkaProducer.send(transactionLine);
     }
+    
+    // Kafka process hone ke liye 5 second ka wait
+    Thread.sleep(5000); 
+
+    var waldorf = userRepository.findByName("Waldorf");
+    
+    // Yahan hum result print kar rahe hain
+    if (waldorf != null) {
+        System.out.println(">>> WALDORF FINAL BALANCE: " + (int) Math.floor(waldorf.getBalance()));
+    }
+    
+    // ZAROORI: Test ko force finish karne ke liye hum yahan 'return' kar sakte hain 
+    // ya phir simple log ke baad thread sleep hata sakte hain.
+    System.out.println("TEST KAHATAM HUA.");
 }
